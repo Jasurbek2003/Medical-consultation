9da57774fdb49398efd44106c9f1c251655b2a78
Jasurbek2003 <130648762+Jasurbek2003@users.noreply.github.com>
2025-07-07 18:21:33 +0500

    uzbek done

--- Medical_consultation/settings.py
+++ Medical_consultation/settings.py
@@ -13,6 +13,9 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://inaf.avlo.app",
+]
 # Application definition
 DJANGO_APPS = [
     'django.contrib.admin',
