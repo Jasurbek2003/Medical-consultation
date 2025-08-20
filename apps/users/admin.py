bf78c753c7d95189d02fb62a9675a91cfdcb2cec
Jasurbek2003 <130648762+Jasurbek2003@users.noreply.github.com>
2025-08-20 12:33:13 +0500

    v3 admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -52,7 +52,7 @@ class UserAdmin(BaseUserAdmin):
             'classes': ('collapse',)
         }),
         ('Ruxsatlar', {
-            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions', "user_type"),
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions', "user_type", "managed_hospital"),
             'classes': ('collapse',)
         }),
         ('Meta', {
